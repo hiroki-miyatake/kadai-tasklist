84becb353c550e09c80ef3525cb7f14de25124e1
hiroki miyatake <mytkhrk0127@yahoo.co.jp>
2017-12-10 13:43:52 +0000

    CRUD tasks

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,19 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::post('tasks', 'TasksController@store')->name('tasks.store');
+// Route::put('tasks/{id}', 'TasksController@update')->name('tasks.update');
+// Route::delete('tasks/{id}', 'TasksController@destroy')->name('tasks.destroy');
+
+// Route::get('tasks', 'TasksController@index')->name('tasks.index');
+
+// Route::get('tasks/create', 'TasksContller@create')->name('tasks.create');
+
+// Route::get('tasks/{id}', 'TasksController@show')->name('tasks.show');
+
+// Route::get('tasks/{id}/edit', 'TasksController@edit')->name('tasks.edit');
+
+
+Route::get('/', 'TasksController@index');
+
+Route::resource('tasks', 'TasksController');
