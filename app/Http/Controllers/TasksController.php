40ae8c23a6c4aedb44c92529f9ae5d98d0102f22
hiroki miyatake <mytkhrk0127@yahoo.co.jp>
2017-12-11 18:44:59 +0000

    add column , validation

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -47,7 +47,13 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'status' =>'required|max:10',
+            'content' => 'required|max:255',
+        ]);
+        
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
@@ -93,8 +99,14 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $this->validate($request, [
+            'status' => 'required|max:10',
+            'content' => 'required|max:255',
+        ]);
+        
         $task = Task::find($id);
-        $task->content = $reqest->content;
+        $task->status = $request->status;
+        $task->content = $request->content;
         $task->save();
         
         return redirect('/');
